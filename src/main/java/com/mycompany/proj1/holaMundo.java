cbb3801899bd5d5b2fd4c1aeabf785acfa722a84
Cristian Parada <cmpmendoza12@gmail.com>
2019-11-23 18:55:24 -0600

    Subiendo el proyecto de prueba Practica en Java a Github

---
+++ src/main/java/com/mycompany/proj1/holaMundo.java
@@ -0,0 +1,22 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proj1;
+
+/**
+ *
+ * @author w2k31984
+ */
+public class holaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola Mundo");
+    }
+    
+}
