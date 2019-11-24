e41fafb9806098547f5e57871a023de4627211c8
Cristian Parada <cmpmendoza12@gmail.com>
2019-11-23 20:10:00 -0600

    Practica sobre variables

--- src/main/java/com/mycompany/proj1/holaMundo.java
+++ src/main/java/com/mycompany/proj1/holaMundo.java
@@ -16,10 +16,30 @@ public class holaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hola Mundo\n");
-        System.out.println("Hola Mundo2");
-        System.out.println("Hola Mundo3");
-        System.out.println("Hola Mundo4");
-    }
-    
+        //System.out.print("Hola Mundo\n");
+        //System.out.println("Hola Mundo2");
+        //System.out.println("Hola Mundo3");
+        //System.out.println("Hola Mundo4");
+        byte mordida;
+        //mordida=126;
+        //Utilizando casting
+        mordida=(byte)126.56;
+        System.out.println("mordida="+mordida);
+        
+        short corto;
+        corto=32000;
+        System.out.println("corto="+corto);
+        
+        int entero;
+        entero=32000;
+        System.out.println("entero="+entero);
+        
+        long largo;
+        largo=32000;
+        System.out.println("largo="+largo);
+        
+        float flotante;
+        flotante=32000;
+        System.out.println("flotante="+flotante);
+ }
 }
